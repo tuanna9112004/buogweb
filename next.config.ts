6d16b22db8ad5bd307083b177d7773cec4acfce4
BUOGS Developer <admin@buogs.com>
2026-08-17 16:28:20 +0700

    Fix middleware 10MB body size limit and rename middleware to proxy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Tăng giới hạn dung lượng body để cho phép upload file audio lên đến 50MB qua Proxy
+  middlewareClientMaxBodySize: '50mb',
 };
 
 export default nextConfig;
